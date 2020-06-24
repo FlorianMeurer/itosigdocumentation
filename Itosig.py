913cbd090e1626328f325391a19a9ff98fb13948
FlorianMeurer <66067683+FlorianMeurer@users.noreply.github.com>
2020-06-24 14:39:12 +0200

    Latest update on the itosig plugin for Olex2 crystallography software program

--- Itosig.py
+++ Itosig.py
@@ -13,14 +13,11 @@ from PluginTools import PluginTools as PT
 import sys
 import plotly 
 import plotly.graph_objects as go
-import numpy as np
 from plotly.subplots import make_subplots as ms
-
+import numpy as np
 
 
 debug = bool(OV.GetParam("olex2.debug", False))
-
-
 instance_path = OV.DataDir()
 
 try:
@@ -70,57 +67,55 @@ class Itosig(PT):
     global hkl_complete_list
     hkl_complete_list = []
     a = open(OV.HKLSrc()).readlines()
-    
+    cell = [float(x) for x in olx.xf.au.GetCell().split(',')]
     
     if a[-1].startswith("E") == True :                 # Checking if INS header is attached, and removes ins header if so
-      print("Ejecting INS header: last 12 lines")
-      a = a[:len(a)-12]
+      print("Ejecting INS header: last 16 lines")
+      a = a[:len(a)-16]
     if a[-1].startswith("_") == True:
-      print("Ejecting INS header: last 14 lines")
-      a = a[:len(a)-15]    
- 
+      print("Ejecting INS header: last 18 lines")
+      a = a[:len(a)-18]    
+    else:
+      a = a[:len(a)-1] 
 
-    cell = [float(x) for x in olx.xf.au.GetCell().split(',')]
     hkl_comp1 = hkl_complete(a, cell)
     file_2 = olex.f('fileOpen("Choose comparison input HKL file", "HKL files|*.hkl",filepath())')
-    
     if file_2 == "":                                  # Checking if a second file is loaded and proceeding with formatting
       pass
     else:
       b = open(file_2).readlines()
       if b[-1].startswith("E") == True:               # Checking if INS header is attached, and removes ins header if so
-        b = b[:len(b)-12]
+        b = b[:len(b)-16]
       if b[-1].startswith("_") == True:
         b = b[:len(b)-18]
+      else:
+        b = b[:len(b)-1]       
       print("END of hkl:", b[-1])
-      hkl_comp2 = hkl_complete(b, cell)               # For now, the compare file uses the EXACT same cell paramenters as the first file
+      hkl_comp2 = hkl_complete(b, cell)               
     
     sorted_inv_dspacings = []
     sorted_Itosig = []
     max_itosigs =  []
+    nr_reflins = []
     for elem in hkl_complete_list:                    # generates a list of sorted d_spacings (lowest to highest)
+      print(elem.hkl_comp)
       zet = ret_entrylist_of_listoflists(elem.hkl_comp, 6)
       sor_zet=[sorted(zet)]
       sorted_inv_dspacings.append(sor_zet)
-      
       ito = ret_entrylist_of_listoflists(elem.hkl_comp, 5)
       sor_ito=[sorted(ito)]
       sorted_Itosig.append(sor_ito)
-      
       max_itosigs.append(max_value(elem.hkl_comp, 5))
-      print(elem.max_values())
+      nr_reflins.append(elem.max_values()[-1])
+      print("Max Int, Max Sigma, Max I/sig, Avg_I/sig, total number of Reflexions used:", elem.max_values())
 
     try:
-      #plot_dspacing_vs_reflins(sorted_inv_dspacings)
-      #plot_itosig_vs_reflins(sorted_Itosig)
-      plotting_everything(sorted_inv_dspacings, sorted_Itosig, 30, max(max_itosigs))
+      plotting_everything(sorted_inv_dspacings, sorted_Itosig, 30, max(max_itosigs), nr_reflins)
     except:
       pass
-      
-      
-      
-def ret_entrylist_of_listoflists(listoflists, i):
-    """Input: list of lists, output: list of entries i of sublist"""
+            
+def ret_entrylist_of_listoflists(listoflists, i):                       # help_funcs for further processing of the lists
+    """Input: list of lists, output: list of float entries i of sublist"""
     ret = []
     for elem in listoflists:
       ret.append(float(elem[i]))
@@ -138,8 +133,8 @@ def avg_list(inplist, i):
   """return the lists i entrys average"""
   counter = 0
   for sublist in inplist:
-    counter += 1 
-  return (sum(float(sublist[i]) for sublist in inplist))/counter
+    counter += 1
+  return (sum(sublist[i] for sublist in inplist))/counter
 
 def calc_d_spacing(h, k, l, cell, Raumgruppe):
   """Calculated the respective d spacing. Params: h, k, l, cellparams, spacegroup"""
@@ -208,10 +203,12 @@ def calc_d_spacing(h, k, l, cell, Raumgruppe):
     if Raumgruppe == "trigonal":
       print("this should be done by now, but isnt")
     if Raumgruppe == "hexagonal":
-      print("this should be done by now, but isnt")    
+      dstar = (0.75*(h**2+k**2+h*k)+l**2*(a/c)**2)*1/a**2
+      dspace = (1/dstar)**0.5
+      return dspace
 
 def gen_hkl_comp(inp, cell):
-    """generates a list with the format of h, k, l, I, sig, I/sig, d-spacing"""
+    """generates a list with the format of h, k, l, I, sig, I/sig, 1/d-spacing"""
     b = inp
     cell_params = cell
     SG = ""
@@ -230,56 +227,20 @@ def gen_hkl_comp(inp, cell):
     else:
       SG = "triklinic"
     ret = []
+    counter = 1
     for i in range(len(b)-1):
       if b[i][0] != "0" and b[i][1] != "0" and b[i][2] != "0" and b[i][3] != "0.00" and b[i][4] != "0.00" and b[i][5] != "0.00":
-        c = []
-        c = b[i][:4].split() + b[i][5:9].split() + b[i][9:12].split() + b[i][12:21].split() + b[i][21:30].split()
-        
-        c.append(float(b[i][12:21].split()[0]) / float(b[i][21:31].split()[0]))
-        c.append(1/(calc_d_spacing(float(c[0]), float(c[1]), float(c[2]), cell, SG)))
-        ret.append(c)             
+          c = []
+          c = b[i][:4].split() + b[i][5:9].split() + b[i][9:13].split() + b[i][12:21].split() + b[i][21:29].split()
+          c.append(float(b[i][12:21].split()[0]) / float(b[i][21:31].split()[0]))
+          c.append(1/(calc_d_spacing(float(c[0]), float(c[1]), float(c[2]), cell, SG)))
+          ret.append(c)
+          counter += 1
       else: 
         continue       
-    return ret   
+    return [ret, counter]   
 
-  
-def plot_dspacing_vs_reflins(reflins_inv_dspac):
-  fig = go.Figure()
-  count=0
-  for x in reflins_inv_dspac: 
-
-    if count == 0:
-      name = "Alpha"
-    else:
-      name = "Beta"
-    y = np.arange(len(x[0]))
-    fig.add_trace(go.Scatter(x=x[0], y=y, name = name))
-    count+=1
-    
-  fig.show()  
-
-def plot_itosig_vs_reflins(itosig):
-  print("Jeah")
-  fig2 = go.Figure()
-  count=0
-  for x in itosig: 
-
-    if count == 0:
-      name = "Alpha"
-    else:
-      name = "Beta"
-    y = np.arange(len(x[0]))
-    fig.add_trace(go.Scatter(x=x[0], y=y, name = name))
-    count+=1
-  
-  annotations = []
-  annotations.append(dict(text="Itosig Plot"))
-  fig2.update_layout(annotations=annotations)
-  
-  fig2.update_layout(showlegend=True)  
-  fig2.show()
-
-def plotting_everything(reflins_inv_dspac, itosig, maxItosig, bins):
+def plotting_everything(reflins_inv_dspac, itosig, bins, maxItosig, nrs):
   """Takes sorted lists of dspacing and I/sig values and creates plots of both vs numbers of reflexions"""
   fig = ms(rows = 2, cols = 2, subplot_titles=("d-spacing VS number of reflins", "Itosig vs Number of reflins", "Test3", "Test4"))
   count = 0
@@ -293,54 +254,52 @@ def plotting_everything(reflins_inv_dspac, itosig, maxItosig, bins):
     fig.add_trace(go.Scatter(x=x[0], y=y, name = name), row=1, col=1)
     count+=1
   
-  count = 0
-  
+  count = 0 
   for elem in itosig:
     for x in reflins_inv_dspac:
       if count == 0:
         name ="Alpha"
       else:
-        name = "Beta"    
+        name = "Beta"
+    rejections = 0
     for i in range(len(elem[0])):
-      if elem[0][i] > 1:
+      if elem[0][i] > 0.1:
         elem[0][i] = math.log10(elem[0][i])
       else:
-        elem[0][i] = 0
-    
-    
-    fig.add_trace(go.Histogram(x=elem[0], histnorm ="percent", name = name, xbins= dict(start = 0, end = maxItosig, size = maxItosig/bins)), row = 1, col = 2)
+        rejections += 1
+    print('While plotting,', rejections, "reflexions have ben rejected due to very low I/sig (< 0.1) for the", name, "dataset")
+        
+    fig.add_trace(go.Histogram(x=elem[0], name = name, xbins= dict(start = 0, end = math.log10(maxItosig), size = math.log10(maxItosig)/bins)), row = 1, col = 2)
     count+=1
     
   fig.update_xaxes(title_text = "1/dspacing", row=1, col=1)
   fig.update_yaxes(title_text = "Number of reflexions", row=1, col=1)
   fig.update_xaxes(title_text = "binned log(I/sig)", row=1, col=2)
-  fig.update_yaxes(title_text = "Number of reflexions", row=1, col=2)  
-    
-  fig.show()
-
+  fig.update_yaxes(title_text = "Number of reflexions", row=1, col=2)
+  fig.update_layout(height=1500, width=2000)
+  
+  try:
+    fig.show()
+  except:
+    print("plotting not successfull")
 
 class hkl_complete:
        
     def __init__(self, file, cell_params):
       self.file = file
       self.cell_params = cell_params
-      self.hkl_comp = gen_hkl_comp(file, cell_params)
+      x = gen_hkl_comp(file, cell_params)
+      self.hkl_comp = x[0]
+      self.nr_reflins_used = x[1]
       self.hkl_comp.pop()
       self.max_int = max_value(self.hkl_comp, 3)
       self.max_sig = max_value(self.hkl_comp, 4)
       self.max_itosig = max_value(self.hkl_comp, 5)
-      self.calc_itosig = avg_list(self.hkl_comp, 6)
+      self.calc_avg_itosig = avg_list(self.hkl_comp, 6)
       hkl_complete_list.append(self)
-    
-    
-    def reflIns_vs_dspacing_plot(self):
-      uns = []
-      uns.append(ret_entrylist_of_listoflists(self.hkl_comp[6]))
-      # print(uns)
-      return
      
     def max_values(self):
-      return [self.max_int, self.max_sig, self.max_itosig]
+      return [self.max_int, self.max_sig, self.max_itosig, self.calc_avg_itosig, self.nr_reflins_used]
     
     def __call__(self):
       return self.hkl_comp
